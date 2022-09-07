80ea7da7ccdb6dd8f6a849123dbb02c19e413e61
berna39 <kalemajoseph39@gmail.com>
2022-09-07 10:39:58 +0200

    Adding is prime service computing

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const app = require('express')();
 const { fork  } = require('child_process');
 
 app.get('/test', (_, res) => res.send('Test Passed'));
+
 app.get('/double/:number', (req, res) => {
     const child_process = fork('./service/calculateDouble.js');
     child_process.send({ "number": parseInt(req.params.number) });
     child_process.on("message", message => res.send(message));
+});
+
+app.get('/isprime/:number', (req, res) => {
+
 });
 
 app.listen(3000, () => console.log("Server up and running"));
---
+++ service/isPrime.js
@@ -0,0 +1,24 @@
+exports.isPrime = (number) => {
+
+    startTime = new Date();
+    endTime = new Date();
+    isPrime = true;
+
+    for(let i = 3; i < number; i++){
+
+        if(number % i === 0){
+            endTime = new Date();
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime) endTime = new Date();
+
+    return {
+        "number": number,
+        "time": endTime.getTime() - startTime.getTime(),
+        "isPrime": isPrime
+    };
+
+}
